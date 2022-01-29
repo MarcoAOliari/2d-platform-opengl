9996b8dcd207fb2557c80d02bd525be9755da5a5
“MarcoAOliari” <marcoaoliari@gmail.com>
2022-01-28 21:35:23 -0300

    mouse mexe no meio do pulo

--- client/main.cpp
+++ client/main.cpp
@@ -43,6 +43,7 @@ int main(int argc, char *argv[]) {
     glutTimerFunc(1000.0/FPS, idle, 0);
     glutKeyboardUpFunc(keyup);
     glutMouseFunc(mouse);
+    glutMotionFunc(motion);
     glutPassiveMotionFunc(motion);
     
     init();
